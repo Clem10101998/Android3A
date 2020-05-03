da4368b51d8b872a5e469e19e59c8c2af2e690fb
Clem10101998 <clement10101998@gmail.com>
2020-05-03 15:14:31 +0200

    Affichage des données reçues du serveur

--- app/src/main/java/com/github/clem10101998/android3a/ListAdapter.java
+++ app/src/main/java/com/github/clem10101998/android3a/ListAdapter.java
@@ -12,7 +12,7 @@ import com.github.clem10101998.android3a.R;
 import java.util.List;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
-    private List<String> values;
+    private List<Pokemon> values;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -31,7 +31,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
         }
     }
 
-    public void add(int position, String item) {
+    public void add(int position, Pokemon item) {
         values.add(position, item);
         notifyItemInserted(position);
     }
@@ -42,7 +42,8 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public ListAdapter(List<String> myDataset) {
+    public ListAdapter(List<Pokemon> myDataset) {
+
         values = myDataset;
     }
 
@@ -63,8 +64,8 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     public void onBindViewHolder(ViewHolder holder, final int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        final String name = values.get(position);
-        holder.txtHeader.setText(name);
+        final Pokemon currentPokemon = values.get(position);
+        holder.txtHeader.setText(currentPokemon.getName());
         holder.txtHeader.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,7 +73,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
             }
         });
 
-        holder.txtFooter.setText("Footer: " + name);
+        holder.txtFooter.setText(currentPokemon.getUrl());
     }
 
     // Return the size of your dataset (invoked by the layout manager)
--- app/src/main/java/com/github/clem10101998/android3a/MainActivity.java
+++ app/src/main/java/com/github/clem10101998/android3a/MainActivity.java
@@ -28,8 +28,8 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-private static final String BASE_URL = "https://pokeapi.co/";
-   //private static final String BASE_URL = "https://raw.githubusercontent.com/Clem10101998/Android3A/master/";
+//private static final String BASE_URL = "https://pokeapi.co/";
+   private static final String BASE_URL = "https://raw.githubusercontent.com/Clem10101998/Android3A/master/";
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -38,23 +38,23 @@ private static final String BASE_URL = "https://pokeapi.co/";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
        //Toast.makeText(getApplicationContext(), "Welcome", Toast.LENGTH_SHORT).show();
-        showList();
+        //showList();
         makeApiCall();
     }
 
-    private void showList() {
+    private void showList(List<Pokemon> pokemonList) {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        List<String> input = new ArrayList<>();
+        /*List<String> input = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             input.add("Test" + i);
-        }
+        }*/
         // define an adapter
-        mAdapter = new ListAdapter(input);
+        mAdapter = new ListAdapter(pokemonList);
         recyclerView.setAdapter(mAdapter);
 
     }
@@ -80,7 +80,8 @@ private static final String BASE_URL = "https://pokeapi.co/";
                 if(response.isSuccessful() && (response.body() != null)) {
 
                     List<Pokemon> pokemonList = response.body().getResults();
-                    Toast.makeText(getApplicationContext(), "API Success", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(getApplicationContext(), "API Success", Toast.LENGTH_SHORT).show();
+                    showList(pokemonList);
                 }else{
                     showError();
                     }
